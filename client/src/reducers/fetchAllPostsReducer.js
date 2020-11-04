1373c7753984252a591160bf60e3b901c6c20fa6
احمد جمعه <engahmedgomaa97@gmail.com>
2020-11-04 06:01:46 +0200

    removing the consoles

--- client/src/reducers/fetchAllPostsReducer.js
+++ client/src/reducers/fetchAllPostsReducer.js
@@ -1,7 +1,6 @@
 export default function (state=[], action) {
     switch (action.type) {
         case 'FETCH_POSTS':
-            console.log(action.payload);
             return action.payload
         default:
             return state
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,16 +1,12 @@
 export default function(state= null, action) {
     switch (action.type) {
         case 'CREATE_NEW_USER':
-            console.log(action.payload);
             return action.payload
         case 'LOG_USER_IN':
-            console.log(action.payload);
             return action.payload;
         case 'FETCHING_USER':
-            console.log(action.payload)
             return action.payload
         case 'LOG_USER_OUT':
-            console.log(action.payload);
             return action.payload
         default:
             return state
